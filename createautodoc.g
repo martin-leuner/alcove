5f579439be2b2bc27f283b1148fe62b5ddf8e60d
Martin Leuner <leuner@momo.math.rwth-aachen.de>
2013-02-01 10:45:25 +0100

    added some documentation

--- createautodoc.g
+++ createautodoc.g
@@ -2,10 +2,11 @@ LoadPackage( "AutoDoc" );
 
 CreateAutomaticDocumentation( "alcove", "gap/AutoDocEntries.g", "doc/", true,
 	[
-		[ "Matroids", "Constructions", Concatenation( [ "This section describes the basic constructions to create matroids using alcove.",
-						" This entails constructors to obtain new matroids as well as the creation of duals and minors." ] ) ],
-		[ "Matroids", "Bases,_circuits_and_all_their_companions", Concatenation( [ "Some introductory text to this section.",
-						"" ] ) ]
+		[ "Matroids", "Construction", Concatenation( [ "This section describes the basic constructions to create matroids using alcove.",
+						" It covers constructors to obtain new matroids as well as the creation of duals, minors and sums." ] ) ],
+		[ "Matroids", "Accessing_attributes", "This section list methods enabling safe access of stored attributes." ],
+		[ "Matroids", "Bases,_circuits_and_their_companions", Concatenation( [ "In this section, methods to compute data such as bases, circuits,",
+											" certain flats or invariants like the Tutte polynomial are listed." ] ) ]
 	] );
 
 QUIT;
--- gap/Matroid.gd
+++ gap/Matroid.gd
@@ -30,22 +30,41 @@ DeclareOperation( "Matroid",
 DeclareOperation( "Matroid",
 		[ IsGeneralizedRowVector and IsNearAdditiveElementWithInverse and IsAdditiveElement ] );
 
-DeclareOperation( "Matroid",
-		[ IsHomalgMatrix ] );
+DeclareOperationWithDocumentation( "Matroid",
+				[ IsHomalgMatrix ],
+				[ "Returns the vector matroid corresponding to the homalg matrix <A>h</A> over some homalg field." ],
+				"a vector matroid",
+				"h",
+				[ "Matroids", "Construction" ]
+			);
 
 DeclareOperation( "MatroidNL",
 		[ IsHomalgMatrix ] );
 
 ## Abstract matroids:
 
-DeclareOperation( "MatroidByBases",
-		[ IsList, IsList ] );
+DeclareOperationWithDocumentation( "MatroidByBases",
+				[ IsList, IsList ],
+				[ "Returns the abstract matroid having the list <A>gset</A> as ground set and the list <A>bases</A> as list of bases. ",
+				"The method checks the base exchange axiom. To construct an abstract matroid with bases without any checks, ",
+				"use the method MatroidByBasesNCL." ],
+				"an abstract matroid with bases",
+				"gset,bases",
+				[ "Matroids", "Construction" ]
+			);
 
 DeclareOperation( "MatroidByBasesNCL",
 		[ IsList, IsList ] );
 
-DeclareOperation( "MatroidByBases",
-		[ IsInt, IsList ] );
+DeclareOperationWithDocumentation( "MatroidByBases",
+				[ IsInt, IsList ],
+				[ "Returns the abstract matroid on <A>n</A> elements having the list <A>bases</A> as list of bases. ",
+				"The method checks the base exchange axiom. To construct an abstract matroid with bases without any checks, ",
+				"use the method MatroidByBasesNCL." ],
+				"an abstract matroid with bases",
+				"n,bases",
+				[ "Matroids", "Construction" ]
+			 );
 
 DeclareOperation( "MatroidByBasesNCL",
 		[ IsInt, IsList ] );
@@ -62,14 +81,28 @@ DeclareOperation( "MatroidByIndependenceFunction",
 DeclareOperation( "MatroidByIndependenceFunctionNCL",
 		[ IsInt, IsFunction ] );
 
-DeclareOperation( "MatroidByCircuits",
-		[ IsList, IsList ] );
+DeclareOperationWithDocumentation( "MatroidByCircuits",
+				[ IsList, IsList ],
+				[ "Returns the abstract matroid having the list <A>gset</A> as ground set and the list <A>circs</A> as list of circuits. ",
+				"The method checks the circuit elimination axiom. To construct an abstract matroid with circuits without any checks, ",
+				"use the method MatroidByCircuits." ],
+				"an abstract matroid with circuits",
+				"gset,circs",
+				[ "Matroids", "Construction" ]
+			);
 
 DeclareOperation( "MatroidByCircuitsNCL",
 		[ IsList, IsList ] );
 
-DeclareOperation( "MatroidByCircuits",
-		[ IsInt, IsList ] );
+DeclareOperationWithDocumentation( "MatroidByCircuits",
+				[ IsInt, IsList ],
+				[ "Returns the abstract matroid on <A>n</A> elements having the list <A>circs</A> as list of circuits. ",
+				"The method checks the circuit elimination axiom. To construct an abstract matroid with circuits without any checks, ",
+				"use the method MatroidByCircuits." ],
+				"an abstract matroid with circuits",
+				"n,circs",
+				[ "Matroids", "Construction" ]
+			);
 
 DeclareOperation( "MatroidByCircuitsNCL",
 		[ IsInt, IsList ] );
@@ -91,11 +124,23 @@ DeclareOperation( "MatroidByRankFunctionNCL",
 
 ## Special matroids:
 
-DeclareOperation( "RandomVectorMatroidOverPrimeField",
-		[ IsInt, IsInt, IsInt ] );
+DeclareOperationWithDocumentation( "RandomVectorMatroidOverPrimeField",
+				[ IsInt, IsInt, IsInt ],
+				[ "Returns the vector matroid corresponding to a random <A>k</A> by <A>n</A> matrix ",
+				"over the prime field of characteristic <A>p</A>. This method is intended mainly ",
+				"for testing purposes." ],
+				"a vector matroid",
+				"k,n,p",
+				[ "Matroids", "Construction" ]
+			);
 
-DeclareOperation( "UniformMatroid",
-		[ IsInt, IsInt ] );
+DeclareOperationWithDocumentation( "UniformMatroid",
+				[ IsInt, IsInt ],
+				[ "Returns the rank <A>k</A> uniform matroid on <A>n</A> elements as an abstract matroid with known rank function." ],
+				"an abstract matroid",
+				"k,n",
+				[ "Matroids", "Construction" ]
+			);
 
 DeclareOperation( "UniformMatroidNL",
 		[ IsInt, IsInt ] );
@@ -121,7 +166,7 @@ DeclareAttributeWithDocumentation( "DualMatroid",
 		 		[ "Computes the dual matroid of <A>mat</A>. If possible, it will be given in the same representation." ],
 				"a matroid",
 				"mat",
-				[ "Matroids", "Constructions" ]
+				[ "Matroids", "Construction" ]
 			);
 
 DeclareAttribute( "ParentAttr",
@@ -130,8 +175,15 @@ DeclareAttribute( "ParentAttr",
 DeclareAttribute( "SimplifiedMatroid",
 		IsMatroid );
 
-DeclareAttribute( "NormalFormOfVectorMatroid",
-		IsMatroid );
+DeclareAttributeWithDocumentation( "NormalFormOfVectorMatroid",
+				IsMatroid,
+				[ "Computes the reduced row echelon form of the underlying matrix of the vector matroid <A>mat</A> ",
+				"and returns its columns other than the first occurence of each unit vector along with the ",
+				"corresponding column labels. This attribute is mainly intended for internal use." ],
+				"a list",
+				"mat",
+				[ "Matroids", "Accessing_attributes" ]
+			);
 
 DeclareOperation( "GroundSet",
 		[ IsMatroid ] );
@@ -156,7 +208,7 @@ DeclareAttributeWithDocumentation( "Bases",
 				[ "Computes the bases of <A>mat</A>. For larger matroids this may take a very long time." ],
 				"a list of bases",
 				"mat",
-				[ "Matroids", "Bases,_circuits_and_all_their_companions" ]
+				[ "Matroids", "Bases,_circuits_and_their_companions" ]
 			);
 
 DeclareAttribute( "KnownBases",
@@ -169,7 +221,7 @@ DeclareAttributeWithDocumentation( "Circuits",
 				  "algorithm, so for matroids with many circuits this may take a long time." ],
 				"a list of circuits",
 				"mat",
-				[ "Matroids", "Bases,_circuits_and_all_their_companions" ]
+				[ "Matroids", "Bases,_circuits_and_their_companions" ]
 			);
 
 DeclareAttribute( "KnownCircuits",
@@ -235,15 +287,20 @@ DeclareProperty( "IsConnected",
 DeclareOperation( "SomeBasis",
 		[ IsMatroid ] );
 
-DeclareOperation( "MatrixOfVectorMatroid",
-		[ IsMatroid ] );
+DeclareOperationWithDocumentation( "MatrixOfVectorMatroid",
+				[ IsMatroid ],
+				[ "Returns the homalg matrix which was used to define the vector matroid <A>mat</A>." ],
+				"a homalg matrix",
+				"mat",
+				[ "Matroids", "Accessing_attributes" ]
+			);
 
 DeclareOperationWithDocumentation( "Minor",
 				[ IsMatroid, IsList, IsList ],
 				[ "Computes the minor <A>mat</A> \\ <A>del</A> / <A>con</A> where <A>del</A> and <A>con</A> are subsets of the ground set of <A>mat</A>." ],
 				"a matroid",
 				"mat,del,con",
-				[ "Matroids", "Constructions" ]
+				[ "Matroids", "Construction" ]
 			);
 
 DeclareOperation( "MinorNL",
@@ -254,7 +311,7 @@ DeclareOperationWithDocumentation( "Deletion",
 				[ "Computes the minor <A>mat</A> \\ <A>del</A> where <A>del</A> is a subset of the ground set of <A>mat</A>." ],
 				"a matroid",
 				"mat,del",
-				[ "Matroids", "Constructions" ]
+				[ "Matroids", "Construction" ]
 			);
 
 DeclareOperationWithDocumentation( "Contraction",
@@ -262,7 +319,7 @@ DeclareOperationWithDocumentation( "Contraction",
 				[ "Computes the minor <A>mat</A> / <A>con</A> where <A>con</A> is a subset of the ground set of <A>mat</A>." ],
 				"a matroid",
 				"mat,con",
-				[ "Matroids", "Constructions" ]
+				[ "Matroids", "Construction" ]
 			);
 
 DeclareOperation( "IsMinor",
@@ -274,7 +331,7 @@ DeclareOperationWithDocumentation( "DirectSumOfMatroids",
 				"DirectSumOfMatroids silently applies DirectSumDecomposition to its arguments." ],
 				"a matroid",
 				"M1,M2",
-				[ "Matroids", "Constructions" ]
+				[ "Matroids", "Construction" ]
 			);
 
 
