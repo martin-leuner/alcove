3ee53355d955b4df99fa650aa150710d303498a0
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2013-10-19 16:35:26 +0200

    the methods for TuttePolynomial now work with two ring elements
    these methods also set the attribute TuttePolynomial once
    these two ring elements are *identical* with the standard
    indeterminates returned by IndeterminatesOfTuttePolynomial( )

--- gap/Matroid.gd
+++ gap/Matroid.gd
@@ -378,25 +378,28 @@ DeclareGlobalFunctionWithDocumentation( "IndeterminatesOfTuttePolynomial",
         [ "Matroids", "Bases,_circuits_and_their_companions" ]
         );
 
-DeclareOperation( "TuttePolynomial",
-                  [ IsMatroid, IsRingElement, IsRingElement ] );
+DeclareAttribute( "TuttePolynomial",
+                  IsMatroid );
 
-DeclareAttributeWithDocumentation( "TuttePolynomial",
-                                IsMatroid,
-                                [ "Computes the Tutte polynomial of <A>mat</A>. The method uses an exponential ",
-                                  "time algorithm, so the computation is feasible only for matroids on very few ",
-                                  "elements. Moreover, it is much faster for vector matroids than for isomorphic ",
-                                  "abstract matroids." ],
-                                "a polynomial in Z[x,y]",
-                                "mat",
-                                [ "Matroids", "Bases,_circuits_and_their_companions" ]
+DeclareOperationWithDocumentation( "TuttePolynomial",
+                                [ IsMatroid, IsRingElement, IsRingElement ],
+                                [ "Computes the Tutte polynomial of the matroid <A>mat</A> as a polynomial expression",
+                                  "in &ZZ;<M>[</M><A>x</A>,<A>y</A><M>]</M>. The method uses an exponential",
+                                  "time algorithm, so the computation is feasible only for matroids on very few",
+                                  "elements. Moreover, it is much faster for vector matroids than for isomorphic",
+                                  "abstract matroids. If the ring elements <A>x</A>,<A>y</A> are not specified",
+                                  "(as second and third argument) predefined indeterminates over &ZZ; will be used instead." ],
+                                "a polynomial in &ZZ;<M>[</M><A>x</A>,<A>y</A><M>]</M>",
+                                "mat, [x, y]",
+                                [ "Matroids", "Bases,_circuits_and_their_companions" ],
+                                rec( function_label := "for IsMatroid, IsRingElement, IsRingElement" )
                         );
 
 DeclareAttributeWithDocumentation( "RankGeneratingPolynomial",
                                 IsMatroid,
                                 [ "Computes the rank generating polynomial (or Whitney polynomial) of <A>mat</A>. ",
                                   "This method calls TuttePolynomial, so the same comments on its runtime apply." ],
-                                "a polynomial in Z[x,y]",
+                                "a polynomial in &ZZ;<M>[</M><A>x</A>,<A>y</A><M>]</M>",
                                 "mat",
                                 [ "Matroids", "Bases,_circuits_and_their_companions" ]
                         );
