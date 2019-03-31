0541e0a79f7c0a75fecceb5b4b0972e83fe00282
Martin Leuner <martin.leuner@rwth-aachen.de>
2019-03-31 14:00:58 +0200

    declared and installed ModuliSpaceOfMatroidByEquationsAndInequations

--- gap/ModuliSpace.gd
+++ gap/ModuliSpace.gd
@@ -11,3 +11,13 @@
 #! @Returns a list of listlists
 DeclareOperation( "MatrixForMatroidRepresentation",
         [ IsMatroid, IsHomalgRing ] );
+
+#! @Description
+#!  Compute the list of equations and inequations defining
+#!  the possibly empty moduli space of vector matroids of the given <A>matroid</A>
+#!  over the ring <A>R</A>.
+#!  This is a quasi-affine representation of the (abstractly) affine moduli space.
+#! @Arguments matroid, R
+#! @Returns a list of listlists
+DeclareOperation( "ModuliSpaceOfMatroidByEquationsAndInequations",
+        [ IsMatroid, IsHomalgRing ] );
